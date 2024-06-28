3abe36573e30b3c08403fb29322e90a3af14fedf
patrickryan331 <patrickryan331@gmail.com>
2024-06-27 22:58:37 -0400

    FSDI 108 React

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import './styles/footer.css';
+
+function Footer(){
+    return(
+        <div className='footer'>
+            <footer className ='content'>
+                <div>
+                    <span className='footerspan'>Learning React. 2024 © All Rights Reserved</span>
+                </div>
+                <div>
+                    <span className='footerspan'>I Am Learning React!!!</span>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/pages/catalog.jsx
@@ -0,0 +1,14 @@
+import './styles/catalog.css';
+
+
+function Catalog(){
+    return(
+        <div>
+            <h1>Hello World! This is React!</h1>
+        </div>
+    );
+
+}
+
+
+export default Catalog;
